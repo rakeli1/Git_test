9eaabe551def410b8651757c3d1937eb7fee7d48
rakeli <rakeliit@protonmail.com>
2024-07-11 16:07:13 +0300

    add1

---
+++ Git_Test/Git_Test.cpp
@@ -0,0 +1,11 @@
+﻿// Git_Test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    int s = 9;
+}
+
